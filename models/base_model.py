5cbbb9375c3bf8c81f2aec5f2e0c1a216f4807b8
actatum88 <alexandra.c.tatum@gmail.com>
2022-10-01 12:47:35 -0500

    Task 5--Adding args & kwargs & dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -7,12 +7,39 @@ timestamp = "%Y-%m-%dT%H:%M:%S.%f"
 
 
 class BaseModel:
-    """ Creates Base Model, define attributes for project """
-    def __init__(self, uuid4):
-        """ Initializes base instance """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    """ Creates Base Model, define attributes for project. """
+    def __init__(self, *args, **kwargs):
+        """ Initializes base instance. """
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at':
+                    self.__dict__[key] = datetime.datetime.strptime(value,
+                                                                    timestamp)
+                if key == 'updated_at':
+                    self.__dict__[key] = datetime.datetime.strptime(value,
+                                                                    timestamp)
+                if key != '__class__':
+                    setattr(self, key, kwargs[key])
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
+            self.updated_at = datetime.datetime.now()
+        self.new = models.storage
+
     def __str__(self):
-        """ Overriding __str__ to print custom string """
-        return ([<class name>] (<self.id>) <self.__dict__>)
+        """ Converts to message """
+        message = "[{}] ({}) {}".format(self.__class__, self.id, self.__dict__)
+        return message
+
+    def save(self):
+        """ Updates public instance to current date/time """
+        self.updated_at = datetime.datetime.now()
+        self.save = models.storage
+
+    def to_dict(self):
+        """ Returing a dictionary containing all keys/values of __dict__ """
+        dict_cpy = self.__dict__.copy()
+        dict_cpy['__class__'] = type(self).__class__.__name__
+        dict_cpy['created_at'] = self.created_at.isoformat()
+        dict_cpy['updated_at'] = self.updated_at.isoformat()
+        return dict_cpy
