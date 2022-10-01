47af8662f14c327d6b3e7bf336ceb5efb5ab8941
actatum88 <alexandra.c.tatum@gmail.com>
2022-10-01 13:15:31 -0500

    Test task 4

--- models/base_model.py
+++ models/base_model.py
@@ -2,44 +2,26 @@
 """ Base model Module """
 
 import datetime
-import uuid
+import uuid import uuid4
 timestamp = "%Y-%m-%dT%H:%M:%S.%f"
 
 
 class BaseModel:
     """ Creates Base Model, define attributes for project. """
-    def __init__(self, *args, **kwargs):
-        """ Initializes base instance. """
-        if kwargs:
-            for key, value in kwargs.items():
-                if key == 'created_at':
-                    self.__dict__[key] = datetime.datetime.strptime(value,
-                                                                    timestamp)
-                if key == 'updated_at':
-                    self.__dict__[key] = datetime.datetime.strptime(value,
-                                                                    timestamp)
-                if key != '__class__':
-                    setattr(self, key, kwargs[key])
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.datetime.now()
-            self.updated_at = datetime.datetime.now()
-        self.new = models.storage
+    def __init__(self):
+        self.id = str(uuid4())
+        self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
 
     def __str__(self):
-        """ Converts to message """
-        message = "[{}] ({}) {}".format(self.__class__, self.id, self.__dict__)
-        return message
+        return "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)
 
     def save(self):
-        """ Updates public instance to current date/time """
         self.updated_at = datetime.datetime.now()
-        self.save = models.storage
 
     def to_dict(self):
-        """ Returing a dictionary containing all keys/values of __dict__ """
-        dict_cpy = self.__dict__.copy()
-        dict_cpy['__class__'] = type(self).__class__.__name__
-        dict_cpy['created_at'] = self.created_at.isoformat()
-        dict_cpy['updated_at'] = self.updated_at.isoformat()
-        return dict_cpy
+        newDict = dict(self.__dict__)
+        newDict.update({"__class__": type(self).__name__})
+        newDict.update({"updated_at": self.updated_at.isoformat()})
+        newDict.update({"created_at": self.created_at.isoformat()})
+        return newDict
