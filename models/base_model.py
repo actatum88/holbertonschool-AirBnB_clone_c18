4f223eca5f95e92492b02325bb2e28d5bf86eb99
actatum88 <alexandra.c.tatum@gmail.com>
2022-10-01 13:23:22 -0500

    Update base_model

--- models/base_model.py
+++ models/base_model.py
@@ -14,12 +14,15 @@ class BaseModel:
         self.updated_at = datetime.datetime.now()
 
     def __str__(self):
+           """ Overriding __str__ to print custom string """
         return "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)
 
     def save(self):
+        """ Updates public instance to current date/time """
         self.updated_at = datetime.datetime.now()
 
     def to_dict(self):
+          """ Returing a dictionary containing all keys/values of __dict__ """
         newDict = dict(self.__dict__)
         newDict.update({"__class__": type(self).__name__})
         newDict.update({"updated_at": self.updated_at.isoformat()})
