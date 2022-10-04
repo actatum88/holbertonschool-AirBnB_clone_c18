734d333aebb9672408aa2b984a52bbdede148f87
actatum88 <alexandra.c.tatum@gmail.com>
2022-10-03 23:41:48 -0500

    More tests

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -24,4 +24,15 @@ print("JSON of my_model:")
 for key in my_model_json.keys():
     print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
     
-    
+    @classmethod
+    def setUpClass(cls):
+        """Set up for the doc tests"""
+        cls.base_funcs = inspect.getmembers(BaseModel, inspect.isfunction)
+        
+    def test_pep8_conformance(self):
+        """test for pep8"""
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(
+            ['models/base_model.py', 'tests/test_models/test_base_model.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
