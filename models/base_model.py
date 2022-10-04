f98a30e164b38cf08cb45ae16476044a8dd06949
actatum88 <alexandra.c.tatum@gmail.com>
2022-10-04 11:39:46 -0500

    Fix

--- models/base_model.py
+++ models/base_model.py
@@ -42,7 +42,6 @@ class BaseModel:
     def save(self):
         """Updates the Public Instance Attribute: updated_at with the current datetime"""
         self.updated_at = datetime.now()
-        storage.new(self)
         storage.save()
 
     def to_dict(self):
