f43d5687295819ccd572f98ef8a4bd293c9819fd
Baylee Duncan <90226747+b-duncs@users.noreply.github.com>
2022-10-04 18:51:06 -0500

    updated files

--- models/base_model.py
+++ models/base_model.py
@@ -4,6 +4,8 @@ import models
 from uuid import uuid4
 from datetime import datetime
 
+time_format = "%Y-%m-%dT%H:%M:%S.%f"
+
 
 class BaseModel:
     """
@@ -17,32 +19,32 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Instantiation of BaseModel"""
-        # string - assign with an uuid when an instance is created
-        self.id = str(uuid4())
-        # datetime - assign with the current datetime when an instance is created
-        self.created_at = datetime.now()
-        # datetime - and it will be updated every time you change your object
-        self.updated_at = datetime.now()
-        
-        """ if kwargs:
-            for k, v in kwargs.items():
-                if k == 'created_at' or k == 'updated_at':
-                    v = datetime.strptime(v, "%Y-%m-%dT%H:%M:%S.%f")
-                if k != '__class__':
-                    setattr(self, k, v)
-
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    setattr(self, key, value)
+            if hasattr(self, 'created_at') and type(self.created_at) is str:
+                self.created_at = datetime.strptime(kwargs['created_at'], time_format)
+            if hasattr(self, 'updated_at') and type(self.updated_at) is str:
+                self.updated_at = datetime.strptime(kwargs['updated_at'], time_format)
         else:
-            storage.new(self)
-        """
-        
+            # string - assign with an uuid when an instance is created
+            self.id = str(uuid4())
+            # datetime - assign with the current datetime when an instance is created
+            self.created_at = datetime.now()
+            # datetime - and it will be updated every time you change your object
+            self.updated_at = datetime.now()
+            models.storage.new(self)
+
     def __str__(self):
         """Should Print: [<class name>] (<self.id>) <self.__dict__>"""
-        return "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)
+        return "[{:s}] ({:s}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
         """Updates the Public Instance Attribute: updated_at with the current datetime"""
         self.updated_at = datetime.now()
-        storage.save()
+        models.storage.new(self)
+        models.storage.save()
 
     def to_dict(self):
         """
@@ -56,7 +58,9 @@ class BaseModel:
               create a dictionary representation with “simple object type” of our BaseModel
         """
         hbnb_dict = self.__dict__.copy()
-        hbnb_dict['created_at'] = str(self.created_at.isoformat())
-        hbnb_dict['updated_at'] = self.updated_at.isoformat()
-        hbnb_dict['__class__'] = str(type(self).__name__)
+        if 'created_at' in hbnb_dict:
+            hbnb_dict['created_at'] = self.created_at.isoformat()
+        if 'updated_at' in hbnb_dict:
+            hbnb_dict['updated_at'] = self.updated_at.isoformat()
+        hbnb_dict['__class__'] = self.__class__.__name__
         return hbnb_dict
