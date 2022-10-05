298e3245f4d3812e3e9601f75a22c07957f645db
Baylee Duncan <90226747+b-duncs@users.noreply.github.com>
2022-10-04 19:26:45 -0500

    updated files

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,19 +1,12 @@
 #!/usr/bin/python3
 """Class FileStorage: serializes instances to a JSON file and deserializes JSON file to instances."""
 import json
-from models.base_model import BaseModel
-from models.amenity import Amenity
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.user import User
+from os import path
 
 
 class FileStorage:
     """Class FileStorage with Private Class Attributes and Public Instance Methods"""
 
-    """Instantiation of FileStorage"""
     # string - path to the JSON file (ex: file.json)
     __file_path = "file.json"
     # dictionary - empty but will store all objects by <class name>.id
@@ -25,23 +18,41 @@ class FileStorage:
 
     def new(self, obj):
         """sets in __objects the obj with key <obj class name>.id"""
-        self.__objects["{}.{}".format(obj.__class__.__name__, obj.id)] = obj
+        self.__objects["{}.{}".format(type(obj).__name__, obj.id)] = obj
 
     def save(self):
         """serializes __objects to the JSON file (path: __file_path)"""
-        with open(self.__file_path, mode='w+') as f:
-            return f.write(json.dumps({key: value.to_dict() for key, value in self.__objects.items()}))
+        with open(self.__file_path, 'w') as f:
+            f.write(json.dumps({key: value.to_dict() for key, value
+                                in self.__objects.items()}))
 
     def reload(self):
         """deserializes the JSON file to __objects (only if the JSON file (__file_path) exists ;
         otherwise, do nothing. If the file doesn’t exist, no exception should be raised)"""
+        from models.base_model import BaseModel
+        from models.amenity import Amenity
+        from models.city import City
+        from models.place import Place
+        from models.review import Review
+        from models.state import State
+        from models.user import User
+
         class_name = {
-            "BaseModel": BaseModel, "User": User, "State": State,
-            "City": City, "Amenity": Amenity, "Place": Place,
+            "BaseModel": BaseModel,
+            "User": User,
+            "State": State,
+            "City": City,
+            "Amenity": Amenity,
+            "Place": Place,
             "Review": Review
         }
-        with open(self.__file_path, mode='r') as myFile:
-            json_dict = json.load(myFile)
-        for key, value in json_dict.items():
-            key_class = class_name.get(key.split('.')[0])
-            self.__objects[key] = key_class(**value)
+        if not path.exists(self.__file_path):
+            pass
+        elif path.getsize(self.__file_path) == 0:
+            pass
+        else:
+            with open(self.__file_path, 'r') as f:
+                json_dict = json.load(f)
+            for key, value in json_dict.items():
+                classes = class_name.get(key.split('.')[0])
+                self.__objects[key] = classes(**value)
